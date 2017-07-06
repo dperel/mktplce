9c86cd49a8afb93969ea94cc8e90f2e2b87c2c5d
David Perel <dmperel@gmail.com>
2017-07-06 15:08:14 -0700

    stuff

---
+++ db/migrate/20170706220340_initial.rb
@@ -0,0 +1,101 @@
+Sequel.migration do
+
+    change do
+        create_table(:patients) do
+            primary_key :id
+            String :first_name, null: false
+            String :last_name, null: false
+            Date :dob, null: false
+            TimestampTZ :created_at
+            TimestampTZ :updated_at
+        end
+
+        create_table(:doctors) do
+            primary_key :id
+            String :first_name, null: false
+            String :last_name, null: false
+            Date :dob, null: false
+            TimestampTZ :created_at
+            TimestampTZ :updated_at
+        end
+
+        create_table(:services) do
+            primary_key :id
+            String :name
+            String :group
+            String :restrictions
+            String :procedure_code
+            String  :effective_date
+            TimestampTZ :created_at
+            TimestampTZ :updated_at
+        end
+
+        create_table(:doctor_has_services) do
+            primary_key :id
+            foreign_key :doctor_id, :doctors
+            foreign_key :service_id, :services
+            TimestampTZ :created_at
+            TimestampTZ :updated_at
+        end
+
+        create_table(:appointments) do
+            primary_key :id
+            foreign_key :patient_id, :patients, null: false
+            foreign_key :doctor_id, :doctors, null: false
+            index [:patient_id, :doctor_id], unique: false
+            TimestampTZ :created_at
+            TimestampTZ :updated_at
+        end
+
+        create_table(:appointment_has_services) do
+            primary_key :id
+            foreign_key :appointment_id, :appointments
+            foreign_key :service_id, :services
+            TimestampTZ :created_at
+            TimestampTZ :updated_at
+        end
+
+        create_table(:appointment_reviews) do
+            primary_key :id
+            foreign_key :appointment_id, :appointments
+            String :content
+            TimestampTZ :created_at
+            TimestampTZ :updated_at
+        end
+
+        create_table(:patient_endorsements) do
+            primary_key :id
+            foreign_key :doctor_id, :doctors
+            foreign_key :endorsing_patient_id , :patients
+            String :content
+            TimestampTZ :created_at
+            TimestampTZ :updated_at
+        end
+
+        create_table(:doctor_endorsements) do
+            primary_key :id
+            foreign_key :receiving_doctor_id, :doctors
+            foreign_key :giving_doctor_id, :doctors
+            String :content
+            TimestampTZ :created_at
+            TimestampTZ :updated_at
+        end
+
+        create_table(:likes) do
+            primary_key :id
+            foreign_key :doctor_id, :doctors
+            foreign_key :patient_id, :patients
+            TimestampTZ :created_at
+            TimestampTZ :updated_at
+        end
+
+        create_table(:characteristics) do
+            primary_key :id
+            foreign_key :endorsement_id, :patient_endorsements
+            String :content
+            TimestampTZ :created_at
+            TimestampTZ :updated_at
+        end
+
+    end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+
+# Seed Services
+CSV.foreach(Rails.root.join('db', 'service.csv'), headers: true) { |row| Service.create(group: row[0],restrictions: row[1], name: row[2], procedure_code: row[3], effective_date: row[4]) }
